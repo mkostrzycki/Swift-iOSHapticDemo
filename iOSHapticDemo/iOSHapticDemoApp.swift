2a363912a313137d6afcf5a524b7aed7020a2ad3
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-23 15:35:50 +0200

    Initial Commit

---
+++ iOSHapticDemo/iOSHapticDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHapticDemoApp.swift
+//  iOSHapticDemo
+//
+//  Created by MaćKo on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSHapticDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
